1267b2f1ca4fb3dd3f98496a950226608a4f58a5
vitalik-shuuu <142603532+vitalkashuba@users.noreply.github.com>
2026-03-17 15:05:55 +0200

    чистка дефолтної гілки

--- data_access/models/hotel.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, ForeignKey
-from data_access.database import Base
-
-class Hotel(Base):
-    __tablename__ = "hotels"
-
-    id = Column(Integer, ForeignKey("places.id"), primary_key=True)
-    user_rating = Column(Integer)
-    stars = Column(Integer)
