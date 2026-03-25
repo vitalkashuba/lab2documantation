078d71f5df004d1b37bd7c04396d51f2953841cc
vitalik-shuuu <142603532+vitalkashuba@users.noreply.github.com>
2026-03-25 09:24:09 +0200

    clear: remove all files

--- data_access/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-engine = create_engine("sqlite:///tripadvisor.db")
-
-SessionLocal = sessionmaker(bind=engine)
-
-Base = declarative_base()
--- data_access/models/restaurant.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from data_access.database import Base
-
-class Restaurant(Base):
-    __tablename__ = "restaurants"
-
-    id = Column(Integer, ForeignKey("places.id"), primary_key=True)
-    menu = Column(String)
-    rating = Column(Integer)
